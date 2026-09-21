f5b24dae51334b54b038dd14763dbcd44bc9fc29
Alaa Sayed <83324135+alaa527@users.noreply.github.com>
2026-09-21 18:15:12 +0300

    Update Navbar links to use hashbang URLs

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -8,20 +8,20 @@ function Navbar() {
     <>
 <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
   <div className="container">
-    <a className="navbar-brand h1" href="#"><i className="fa-solid text-danger fa-clapperboard"></i>  Movie</a>
+    <a className="navbar-brand h1" href="#!"><i className="fa-solid text-danger fa-clapperboard"></i>  Movie</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav m-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <a className="nav-link active" aria-current="page" href="#!">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Movies</a>
+          <a className="nav-link" href="#!">Movies</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">TV Shows</a>
+          <a className="nav-link" href="#!">TV Shows</a>
         </li>
     
       </ul>
